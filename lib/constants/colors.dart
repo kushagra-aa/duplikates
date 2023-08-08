6350828372c53194e234e94c549c4f7adc721064
kushagra-aa <kushagraagnihotri24@gmial.com>
2023-08-08 09:39:26 +0530

    added colors constants

---
+++ lib/constants/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const Color bookmyshowLogoColor = Color.fromRGBO(223, 24, 39, 1);
+const Color facebookLogoColor = Color.fromRGBO(59, 89, 152, 1);
+const Color instagramLogoColor = Color.fromRGBO(193, 53, 132, 1);
+const Color linkedinLogoColor = Color.fromRGBO(10, 102, 194, 1);
+const Color messengerLogoColor = Color.fromRGBO(0, 153, 255, 1);
+const Color snapchatLogoColor = Color.fromRGBO(255, 252, 0, 1);
+const Color telegramLogoColor = Color.fromRGBO(0, 136, 204, 1);
+const Color whatsappLogoColor = Color.fromRGBO(37, 211, 102, 1);
+const Color xLogoColor = Color.fromRGBO(0, 0, 0, 1);
