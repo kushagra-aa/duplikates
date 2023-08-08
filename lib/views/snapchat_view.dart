c2f481178170d5b1f52903d24307d9b472b76c0a
kushagra-aa <kushagraagnihotri24@gmial.com>
2023-08-08 08:56:31 +0530

    added basic widget in views

---
+++ lib/views/snapchat_view.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 189, 236, 255),
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Snapchat'),
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Snapchat'),
+          ],
+        ),
+      ),
+    );
+  }
+}
