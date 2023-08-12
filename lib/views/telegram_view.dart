27e9bd0d7e23d815318463b510408548fce1c881
kushagra-aa <kushagraagnihotri24@gmial.com>
2023-08-12 08:39:01 +0530

    added telegram chat card

--- lib/views/telegram_view.dart
+++ lib/views/telegram_view.dart
@@ -1,4 +1,4 @@
-import 'package:duplikates/constants/colors.dart';
+import 'package:duplikates/helpers/genrate_random_image.dart';
 import 'package:flutter/material.dart';
 
 class TelegramPage extends StatelessWidget {
@@ -7,22 +7,100 @@ class TelegramPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: telegramLogoColor,
+      backgroundColor: const Color.fromRGBO(24, 24, 25, 1),
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('Telegram'),
-        leading: BackButton(onPressed: () {
-          Navigator.pop(context);
-        }),
-      ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Telegram'),
-          ],
+        backgroundColor: const Color.fromRGBO(38, 38, 40, 1),
+        title: const Text(
+          'Telegram',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+          ),
         ),
+        // leading: BackButton(onPressed: () {
+        //   Navigator.pop(context);
+        // }),
+        iconTheme: const IconThemeData(color: Colors.white),
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.search,
+              ))
+        ],
       ),
+      body: Column(
+        children: [
+          chatCard(),
+          chatCard(),
+          chatCard(),
+          chatCard(),
+          chatCard(),
+          chatCard(),
+          chatCard(),
+          chatCard(),
+          chatCard(),
+        ],
+      ),
+      drawer: const Placeholder(),
+    );
+  }
+
+  Container chatCard() {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 5),
+      child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CircleAvatar(
+              backgroundImage: NetworkImage(
+                genrateRandomImage(),
+              ),
+            ),
+            const Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('JavaScript Channel',
+                    style: TextStyle(
+                      color: Colors.white,
+                    )),
+                Row(
+                  children: [
+                    Text('Kushagra:',
+                        style: TextStyle(
+                          color: Colors.white,
+                        )),
+                    Text('I think you should try doi...',
+                        style: TextStyle(
+                          color: Colors.white,
+                        )),
+                  ],
+                ),
+              ],
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                const Text("8:13 AM",
+                    style: TextStyle(
+                      color: Colors.white,
+                    )),
+                Container(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 4, vertical: 2),
+                  decoration: const BoxDecoration(
+                      color: Color.fromRGBO(69, 69, 69, 1),
+                      borderRadius: BorderRadius.all(Radius.circular(10))),
+                  child: const Text("1421",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 12,
+                      )),
+                ),
+              ],
+            )
+          ]),
     );
   }
 }
