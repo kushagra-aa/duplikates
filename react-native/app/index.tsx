a9ccf29e055211f3ba2947a5a4c4ca02120416fc
kushagra-aa <kushagraagnihotri24@gmail.com>
2025-08-01 18:31:35 +0530

    Setup: SVG Support

--- react-native/app/index.tsx
+++ react-native/app/index.tsx
@@ -1,3 +1,5 @@
+import { Icon } from "@/components/Icons";
+import AddIcon from "@/icons/ArrowDownIcon.svg";
 import { Text, View } from "react-native";
 
 export default function Index() {
@@ -10,6 +12,7 @@ export default function Index() {
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Icon Icon={AddIcon} color="SnapchatYellow" />
     </View>
   );
 }
---
+++ react-native/components/Icons.tsx
@@ -0,0 +1,22 @@
+import { COLORS } from "./../constants/colors";
+
+type IconProps = {
+  Icon: React.FC<{
+    width: number;
+    height: number;
+    fill: string;
+    stroke: string;
+  }>;
+  size?: number;
+  color?: keyof typeof COLORS;
+};
+
+// Icon Parser
+export const Icon = ({ size = 24, color = "SnapchatRed", Icon }: IconProps) => (
+  <Icon
+    width={size}
+    height={size}
+    fill={COLORS[color]}
+    stroke={COLORS[color]}
+  />
+);
