b71f927b3123c2eeaaef252abc93407cb5ae61db
kushagra-aa <kushagraagnihotri24@gmail.com>
2025-08-05 20:58:45 +0530

    Feature: Empty Social Pages

---
+++ react-native/app/instagram/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function InstagramPage() {
+  return (
+    <SafeAreaView>
+      <Text>InstagramPage</Text>
+    </SafeAreaView>
+  );
+}
